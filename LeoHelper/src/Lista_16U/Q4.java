9a030ec58de099443b7ceebe7598b270eefda819
pedro <phls73376@gmail.com>
2018-11-26 12:17:58 -0300

    q4 L16U

---
+++ LeoHelper/src/Lista_16U/Q4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double a, b, c;
+        System.out.println("Informe o valor de A");
+        a = in.nextDouble();
+        System.out.println("Informe o valor de B");
+        b = in.nextDouble();
+        System.out.println("Informe o valor de C");
+        c = in.nextDouble();
+        double del = delta(a,b,c);
+        if(del < 0){
+            System.out.println("Não existem raízes reais!");
+        }else{
+            double x1 = ((-b) + Math.sqrt(del))/(double)2*a;
+            double x2 = (double)((-b) - Math.sqrt(del))/(double)2*a;
+            System.out.println("As raízes são x1= " +x1 + " x2= " +x2);
+        }
+    }
+
+    private static double delta(double a, double b, double c) {
+        return (b * b) - (4 * (a * c));
+    }
+}
