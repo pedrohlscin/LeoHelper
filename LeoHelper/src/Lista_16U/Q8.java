1bf1b7225f3e35721dd8fd74bdbe70ba08d2549f
pedro <phls73376@gmail.com>
2018-11-26 12:55:08 -0300

    q8 L16U

--- LeoHelper/src/Lista_16U/Q8.java
+++ LeoHelper/src/Lista_16U/Q8.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Q8 {
+    public static void main(String[] args) {
+        int[] cont = new int[7];
+        for(int i = 0; i < 1000000; i++){
+            cont[(int)jogarDado()] += 1;
+        }
+        for(int i = 0; i < 6; i++){
+            System.out.println("A quantidade de vezes que " + (i+1) + " foi sorteado é: :" + cont[i]);
+        }
+    }
+    private static double jogarDado() {
+        return ThreadLocalRandom.current().nextInt(0,6);
+    }
 }
