2a4a529e1700713d7638eddc0f7bac57ffc12b29
pedro <phls@cin.ufpe.br>
2018-11-27 10:02:56 -0300

    alterado para permitir input do usuário

--- LeoHelper/src/Lista_16U/Q9.java
+++ LeoHelper/src/Lista_16U/Q9.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Q9 {
     public static void main(String[] args) {
-        int number = 21;
+        int number;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Informe um valor para sabermos se ele é primo:");
+        number = in.nextInt();
         System.out.println(isPrime(number));
     }
 
